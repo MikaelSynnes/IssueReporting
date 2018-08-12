54256ce370b7355a5937f8922e572de4a16249e4
Mikael <Mikaelsynnes@live.no>
2018-08-12 16:08:27 +0200

    - Removed unused files - Removed unused objects and code

--- src/main/java/hello/API.java
+++ src/main/java/hello/API.java
@@ -6,33 +6,14 @@
 package hello;
 
 import Data.Report;
-import Data.Token;
-import Data.UserDto;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-
-import java.util.UUID;
-import javax.annotation.security.RolesAllowed;
-import javax.enterprise.inject.Model;
-
-import javax.ws.rs.GET;
-
-import javax.ws.rs.Path;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -42,73 +23,43 @@ import org.springframework.web.servlet.view.RedirectView;
  */
 @RestController
 public class API {
-   private final InMemoryUserDetailsManager inMemoryUserDetailsManager;
- 
-    private HashMap<String, Token> accessTokens;
-       private ArrayList<Report> tickets;
 
-  
+    private final InMemoryUserDetailsManager inMemoryUserDetailsManager;
+    private ArrayList<Report> tickets;
+
     @Autowired
     public API(InMemoryUserDetailsManager inMemoryUserDetailsManager) {
-       this.inMemoryUserDetailsManager = inMemoryUserDetailsManager;
+        this.inMemoryUserDetailsManager = inMemoryUserDetailsManager;
         tickets = createDummyTickets();
     }
 
- @RequestMapping("/add")
-    public String add(@RequestParam("username") String username, @RequestParam("password") String password) {
-        UserDetails user=  User.withDefaultPasswordEncoder().username(username).password(password).roles("USER").build();
+    @RequestMapping("/add")
+    public RedirectView add(@RequestParam("username") String username, @RequestParam("password") String password) {
+        UserDetails user = User.withDefaultPasswordEncoder().username(username).password(password).roles("USER").build();
         inMemoryUserDetailsManager.createUser(user);
-      
+
         return new RedirectView("/");
     }
 
-    
+    @RequestMapping("/tickets")
+    public ModelAndView adminPage() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("tickets", tickets);
+        return model;
+    }
 
-      /*  String stringbuilder = null;    
-        for(Report report:createDummyTickets()){
-            stringbuilder+="User: " + report.getUser()+ "Report: " + report.getIssue() + "      ";
-        }
-        return stringbuilder;   
-*/
-    
-@RequestMapping("/tickets")
-public ModelAndView adminPage() {
-    ModelAndView model = new ModelAndView();
- 
-    model.addObject("tickets",tickets);
-
-    return model;
-
-}
-
-@RequestMapping("/createticket/addticket")
-    public RedirectView addTicket(@RequestParam("user") String user,@RequestParam("text") String text){
-        tickets.add(new Report(text,user));
+    @RequestMapping("/createticket/addticket")
+    public RedirectView addTicket(@RequestParam("user") String user, @RequestParam("text") String text) {
+        tickets.add(new Report(text, user));
         return new RedirectView("/");
     }
-    
-
-
-
-
-    @GET
-    @Path("test")
-    public String test() {
-        return "This is a test function";
-    }
-
-   
     private ArrayList<Report> createDummyTickets() {
         ArrayList<Report> reports = new ArrayList();
         reports.add(new Report("This is a dummy issue", "Anonymous"));
         reports.add(new Report("This is a dummy issue 2", "Anonymous"));
         reports.add(new Report("This is a dummy issue 3", "I chose to show name"));
         reports.add(new Report("The ticket system is trash", "Mikael"));
-
         return reports;
     }
 
-  
-
-  
 }
